c374fb9cd8b0c9266632c46b2efc1f2b119e9df9
jjrv <juha@busfaster.com>
2015-08-09 11:20:07 +0300

    Refactor parts into another module.

--- autogypi.js
+++ autogypi.js
@@ -3,75 +3,7 @@
 
 var fs = require('fs');
 var path = require('path');
-
-/** Read configuration file in JSON format.
-  * @param {string} confPath Path of file to read.
-  * @return {Object.<string, *>} File contents. */
-function readConf(confPath) {
-	try {
-		var confData = fs.readFileSync(confPath);
-	} catch(err) {
-		console.error(err);
-		throw('Error reading ' + confPath);
-	}
-
-	try {
-		var conf = JSON.parse(confData);
-	} catch(err) {
-		console.error(err);
-		throw('Error parsing JSON from ' + confPath);
-	}
-
-	return(conf);
-}
-
-/** Module entry point returned by require.resolve might be in a subdirectory.
-  * Look for the module root directory containing the module's packages.json
-  * and hopefully autogypi.json file.
-  * @param {string} modulePath Path to some file or directory inside the module.
-  * @return {{modulePath: string, moduleConfPath: string}} Path to module root
-  *   and possible autogypi.json file. */
-function findModuleRoot(modulePath, depend, confPath) {
-	var nextPath;
-	var moduleConfFound = false;
-	var moduleConfPath;
-	var depth = 0;
-
-	while(1) {
-		moduleConfPath = path.join(modulePath, 'autogypi.json');
-
-		if(fs.existsSync(moduleConfPath)) {
-			// Found autogypi configuration for the module, process it.
-			moduleConfFound = true;
-			break;
-		}
-
-		// Found a package.json file, attempt to process the module.
-		if(fs.existsSync(path.join(modulePath, 'package.json'))) break;
-
-		// This is not the root of the module so move one directory up.
-		nextPath = path.resolve(modulePath, '..');
-
-		// Bail out if we cannot go up any more, or already reached a
-		// node_modules directory which should be outside the module's
-		// directory tree, or we've been stuck going up a number of
-		// levels already.
-		if(
-			nextPath === modulePath
-		||	path.basename(nextPath).toLowerCase() === 'node_modules'
-		||	++depth > 20
-		) {
-			throw('Cannot find package.json for dependency ' + depend + ' referenced in ' + confPath);
-		}
-
-		modulePath = nextPath;
-	}
-
-	return({
-		modulePath: modulePath,
-		moduleConfPath: moduleConfFound ? moduleConfPath : null
-	});
-}
+var link = require('@lib/autolink');
 
 /** @param {string} confPath Configuration file path to show in error messages.
   * @param {Array.<string>} dependList Names of required npm modules.
@@ -80,29 +12,16 @@ function findModuleRoot(modulePath, depend, confPath) {
 function findDepends(confPath, dependList, resolver, result) {
 	var dependNum, dependCount;
 	var depend;
-	var modulePath, moduleConfPath, entryPath, resolverPath;
-	var moduleRootInfo;
+	var modulePath, entryPath, resolverPath;
+	var moduleConfInfo;
 	var moduleConf;
 	var moduleList, modulePathList, rawPathList;
 	var pathTbl;
 
-	function notPathy(name) {
-		return(
-			// Scoped module names start with an @.
-			name.match(/^@/) ||
-			// Names containing no slashes are probably module names.
-			!name.match(/[/\\]/)
-			// Names containing a single slash might be references to packages
-			// published on Github but autogypi doesn't support that, so such
-			// a condition is omitted here.
-		);
-	}
-	function isPathy(name) {return(!notPathy(name));}
-
 	// Divide dependencies into two groups according to whether they look like paths.
 	// Anything that's not a path is assumed to be a resolvable module name.
-	rawPathList = dependList.filter(isPathy);
-	moduleList = dependList.filter(notPathy);
+	rawPathList = dependList.filter(link.isPathy);
+	moduleList = dependList.filter(link.notPathy);
 
 	// Get full paths to modules.
 	try {
@@ -134,7 +53,7 @@ function findDepends(confPath, dependList, resolver, result) {
 		// twice even if it would be found in a different path from inside
 		// another module (multiple copies of a library in a C++ project is
 		// unlikely to work).
-		if(notPathy(depend)) {
+		if(link.notPathy(depend)) {
 			if(result.foundModuleTbl[depend]) continue;
 			result.foundModuleTbl[depend] = true;
 		}
@@ -147,9 +66,12 @@ function findDepends(confPath, dependList, resolver, result) {
 		if(!fs.statSync(entryPath).isDirectory()) entryPath = path.dirname(entryPath);
 
 		// Look for the module's root directory and possible autogypi.conf.
-		moduleRootInfo = findModuleRoot(entryPath, depend, confPath);
-		modulePath = moduleRootInfo.modulePath;
-		moduleConfPath = moduleRootInfo.moduleConfPath;
+		moduleConfInfo = link.findModuleConf(entryPath, ['autogypi.json', 'package.json']);
+		if(!moduleConfInfo) {
+			throw('Cannot find package.json for dependency ' + depend + ' referenced in ' + confPath);
+		}
+
+		modulePath = moduleConfInfo.modulePath;
 
 		// Ensure an identical module has not been included already based on
 		// the full path of its root directory.
@@ -158,10 +80,10 @@ function findDepends(confPath, dependList, resolver, result) {
 
 		console.log('Found dependency ' + depend + ' in ' + modulePath);
 
-		if(moduleConfPath) {
+		if(moduleConfInfo.confName === 'autogypi.json') {
 			// Luckily this module has an autogypi.json file specifying
 			// how it should be included. Parse the configuration file.
-			parseConf(moduleConfPath, resolver, result);
+			parseConf(moduleConfInfo.confPath, resolver, result);
 		} else {
 			// No configuration file found, try to do something useful for
 			// hopefully getting the module included. This is enough for nan.
@@ -176,7 +98,7 @@ function findDepends(confPath, dependList, resolver, result) {
   * @param {*} resolver Like resolver function defined in this file, but possibly executing in the context of another module.
   * @param {{gypi: *}} result Object that forms the output .gypi file when written out as JSON. */ 
 function parseConf(confPath, resolverPrev, result) {
-	var conf = readConf(confPath);
+	var conf = link.readConf(confPath);
 	var dependList = conf['dependencies'];
 	var includeList = conf['includes'];
 	var resolverPath = conf['resolver'];
